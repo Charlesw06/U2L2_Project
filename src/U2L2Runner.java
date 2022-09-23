37e2c4eeaa79b016d1100b2b698145846dff447a
BT_4N2_20 <cwalford3884@bths.edu>
2022-09-23 12:08:04 -0400

    Initial commit

---
+++ src/U2L2Runner.java
@@ -0,0 +1,8 @@
+public class U2L2Runner {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(150, 200);
+        Rectangle square = new Rectangle(100);
+        Rectangle rectDefault = new Rectangle();
+
+    }
+}
