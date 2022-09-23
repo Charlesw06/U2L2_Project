7d0322d8c97b1664164c0afd555d6075282ba979
BT_4N2_20 <cwalford3884@bths.edu>
2022-09-23 12:26:59 -0400

    Added code to calculate perimeter and area of rectangles

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -4,5 +4,22 @@ public class U2L2Runner {
         Rectangle square = new Rectangle(100);
         Rectangle rectDefault = new Rectangle();
 
+        rect1.setWidth(125);
+        square.setWidth(125);
+        square.setLength(125);
+        rectDefault.setWidth(125);
+
+        int perimRect1 = (2 * rect1.getLength()) + (2 * rect1.getWidth());
+        int perimSquare = (4 * square.getLength());
+        int perimRectDefault = (2 * rectDefault.getLength()) + (2 * rectDefault.getWidth());
+        int perimSum = perimRect1 + perimRectDefault + perimSquare;
+
+        int areaRect1 = rect1.calculateArea();
+        int areaSquare = square.calculateArea();
+        int areaRectDefault = rectDefault.calculateArea();
+        int areaSum = areaRect1 + areaRectDefault + areaSquare;
+
+        System.out.println("The total length that needs fencing is " + perimSum + " feet.");
+        System.out.println("The total area that needs seeding is " + areaSum + " square feet.");
     }
 }
